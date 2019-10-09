2aea0c813657903bc177937da49687f1b661e4af
ThelsK <github@thels.nl>
2019-10-09 17:35:28 +0200

    Implement user registration

--- index.js
+++ index.js
@@ -1,31 +1,108 @@
-const { User, Device } = require("./models")
 const server = require("./server")
+const { register } = require("./users")
 
 const connections = []
-let nextID = 1
+const nextId = 1
 
 // Handle websockets.
 server.on("request", req => {
 
   // Accept the request, and add it to the connections.
-  const webSocket = req.accept("json", req.origin)
-  console.log("Connection accepted from:", webSocket.remoteAddress)
-  connections.push(webSocket)
+  const connection = {}
+  connections.push(connection)
+  connection.id = nextId++
 
-  // Set a client ID and return this ID to the client.
-  webSocket.clientID = nextID++
-  webSocket.sendUTF(JSON.stringify({
-    type: "id",
-    id: webSocket.clientID
-  }))
+  connection.webSocket = req.accept("json", req.origin)
+  console.log("Connection accepted from:",
+    connection.webSocket.remoteAddress)
 
-  webSocket.on("message", msg => {
+  connection.webSocket.on("message", async msg => {
     if (msg.type !== "utf8") {
       console.error("Incorrect data:", msg)
       return
     }
 
-    const data = msg.utf8Data
-    console.log("Received data:", data)
+    const data = JSON.parse(msg.utf8Data)
+    switch (data.type) {
+
+      case "register":
+        if (connection.username) {
+          connection.webSocket.sendUTF(JSON.stringify({
+            type: "message",
+            message: `Already logged in as "${connection.username}".`
+          }))
+          return
+        }
+
+        const registerResult = await register(data)
+        if (registerResult.username) {
+          connection.username = registerResult.username
+        }
+        connection.webSocket.sendUTF(JSON.stringify(registerResult))
+        return
+
+      case "login":
+        if (connection.username) {
+          connection.webSocket.sendUTF(JSON.stringify({
+            type: "message",
+            message: `Already logged in as "${connection.username}".`
+          }))
+          return
+        }
+
+        const loginResult = await login(data)
+        if (loginResult.username) {
+          connection.username = loginResult.username
+        }
+        connection.webSocket.sendUTF(JSON.stringify(loginResult))
+        return
+
+      case "device":
+        if (!connection.username) {
+          connection.webSocket.sendUTF(JSON.stringify({
+            type: "message",
+            message: "Please login or register."
+          }))
+          return
+        }
+
+        if (connection.device) {
+          connection.webSocket.sendUTF(JSON.stringify({
+            type: "message",
+            message: `Device already declared as "${connection.device}".`
+          }))
+          return
+        }
+
+        if (!data.device.trim()) {
+          connection.webSocket.sendUTF(JSON.stringify({
+            type: "message",
+            message: "Please include a name for your device.",
+          }))
+          return
+        }
+
+        for (let i = 0; i < connections.length; i++) {
+          if (connection.username === connections[i].username &&
+            data.device.trim().toLowerCase() ===
+            connections[i].device.toLowerCase()) {
+            connection.webSocket.sendUTF(JSON.stringify({
+              type: "message",
+              message: "Devicename already in use for your account.",
+            }))
+          }
+        }
+
+        connection.device = data.device.trim()
+        connection.webSocket.sendUTF(JSON.stringify({
+          type: "device",
+          device: data.device.trim(),
+          message: "Devicename declared.",
+        }))
+        return
+
+      default:
+        console.error("Unknown data:", data)
+    }
   })
 })
---
+++ users.js
@@ -0,0 +1,81 @@
+const Sequelize = require("sequelize")
+const bcrypt = require("bcrypt")
+
+const { User } = require("./models")
+
+const register = async data => {
+  try {
+
+    if (!data.username.trim()) {
+      return {
+        type: "message",
+        message: "Please include a username.",
+      }
+    }
+
+    if (!data.password.trim()) {
+      return {
+        type: "message",
+        message: "Please include a password.",
+      }
+    }
+
+    if (!data.confirmPassword.trim()) {
+      return {
+        type: "message",
+        message: "Please include a password confirmation.",
+      }
+    }
+
+    if (data.username.trim().toLowerCase() ===
+      data.password.trim().toLowerCase()) {
+      return {
+        type: "message",
+        message: "Username cannot be identical to password.",
+      }
+    }
+
+    if (data.password.trim().toLowerCase() !==
+      data.confirmPassword.trim().toLowerCase()) {
+      return {
+        type: "message",
+        message: "Password and confirmation do not match.",
+      }
+    }
+
+    const duplicateUsername = await User.findOne({
+      where: {
+        username: {
+          [Sequelize.Op.iLike]: data.username.trim()
+        }
+      }
+    })
+    if (duplicateUsername) {
+      return {
+        type: "message",
+        message: "Username already exists.",
+      }
+    }
+
+    const encryptedPassword =
+      await bcrypt.hashSync(data.password.trim().toLowerCase(), 10)
+    const user = await User.create({
+      username: data.username.trim(),
+      password: encryptedPassword,
+    })
+    return {
+      type: "login",
+      username: user.username,
+      message: `Registered account ${user.username}`
+    }
+
+  } catch (error) {
+    console.error(error)
+    return {
+      type: "message",
+      message: "Internal server error.",
+    }
+  }
+}
+
+module.exports = { register }
